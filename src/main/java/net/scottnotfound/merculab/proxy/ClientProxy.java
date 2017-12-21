38cd4ed9242acdcf69fbccc4ef847a97055ade3e
narwhalfire <narwhalfire@scottnotfound.net>
2017-12-21 14:38:12 -0500

    some proxy annotations

--- src/main/java/net/scottnotfound/merculab/proxy/ClientProxy.java
+++ src/main/java/net/scottnotfound/merculab/proxy/ClientProxy.java
@@ -6,10 +6,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.scottnotfound.merculab.MercuLabBlocks;
 import net.scottnotfound.merculab.MercuLabCompounds;
 import net.scottnotfound.merculab.MercuLabItems;
 
+@SideOnly(Side.CLIENT)
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
 
--- src/main/java/net/scottnotfound/merculab/proxy/ServerProxy.java
+++ src/main/java/net/scottnotfound/merculab/proxy/ServerProxy.java
@@ -4,7 +4,9 @@ package net.scottnotfound.merculab.proxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.SERVER)
 @Mod.EventBusSubscriber(Side.SERVER)
 public class ServerProxy extends CommonProxy {
 
