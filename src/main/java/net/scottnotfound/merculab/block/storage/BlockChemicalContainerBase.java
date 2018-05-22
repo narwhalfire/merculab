d8aa54d5bca963d6deaeed54797247553877e689
narwhalfire <narwhalfire@scottnotfound.net>
2018-05-22 14:50:43 -0400

    curly brace

--- src/main/java/net/scottnotfound/merculab/block/storage/BlockChemicalContainerBase.java
+++ src/main/java/net/scottnotfound/merculab/block/storage/BlockChemicalContainerBase.java
@@ -3,8 +3,7 @@ package net.scottnotfound.merculab.block.storage;
 import net.minecraft.block.material.Material;
 import net.scottnotfound.merculab.block.BlockChemicalBase;
 
-public abstract class BlockChemicalContainerBase extends BlockChemicalBase
-{
+public abstract class BlockChemicalContainerBase extends BlockChemicalBase {
 
     public BlockChemicalContainerBase(Material material) {
         super(material);
